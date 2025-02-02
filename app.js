9280328a3ee089a02045eb4170cd3fb9dd901516
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2025-02-02 18:18:40 -0300

    Criando a função para criar a tarefa

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+function criarItem(text, status = "") {
+  const item = document.createElement("label");
+  item.classList.add("todo__item");
+  item.innerHTML = `
+    <input type="checkbox" ${status}/>
+    <div>${text}</div>
+    <input type="button" value="X" />
+    `;
+
+  document.getElementById("todoList").appendChild(item);
+}
+
+// criarItem("Criação teste");
